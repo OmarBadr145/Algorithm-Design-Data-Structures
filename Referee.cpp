b0a79d0f1ba6a627672ab47779cac1f91246ac60
OmarBadr145 <omary.badr145@gmail.com>
2024-07-26 14:56:37 +0930

    Finished referee on to testing

--- Referee.cpp
+++ Referee.cpp
@@ -9,9 +9,42 @@ Player * refGame(Player * player1, Player * player2){
     char p1Move = player1->makeMove();
     char p2Move = player2->makeMove();
     
-    
-    if (p1Move == 'r' && p1Move == 'r'){
+    /// all draws possible
+    if (p1Move == 'r' && p2Move == 'r'){
         return nullptr;
     };
-    ///have to do other options as well.
+
+    if (p1Move == 'p' && p2Move == 'p'){
+        return nullptr;
+    };
+
+    if (p1Move == 's' && p2Move == 's'){
+        return nullptr;
+    };
+
+    /// all p1 wins
+    if (p1Move == 'r' && p2Move == 's'){
+        cout << player1->getName() << "Wins!";
+    };
+
+    if (p1Move == 'p' && p2Move == 'r'){
+        cout << player1->getName() << "Wins!";
+    };
+
+    if (p1Move == 's' && p2Move == 'p'){
+        cout << player1->getName() << "Wins!";
+    };
+
+    /// all p2 wins
+    if (p1Move == 'r' && p2Move == 'p'){
+        cout << player2->getName() << "Wins!";
+    };
+
+    if (p1Move == 's' && p2Move == 'r'){
+        cout << player2->getName() << "Wins!";
+    };
+    if (p1Move == 'p' && p2Move == 's'){
+        cout << player2->getName() << "Wins!";
+    };
+
 };
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "Human.h"
+#include "Computer.h"
+#include "Player.h"
+#include "Referee.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    Human human;
+    Computer computer;
+    Player * refGame(Player * human, Player * computer);
+}
