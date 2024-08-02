1c6425577b37b0f154e3941c5439610fab2fae0d
OmarBadr145 <omary.badr145@gmail.com>
2024-08-02 16:00:55 +0930

    Working Game lfg im actiually getting better

--- MoveFactory.cpp
+++ MoveFactory.cpp
@@ -16,38 +16,37 @@
 
 Move *MoveFactory ::CreateMove(std::string moveName)
 {
-    Move* currentMove;
     if (moveName == "Rock")
     {
-        currentMove = new Rock();
+        return (new Rock());
     }
     else if (moveName == "Paper")
     {
-        currentMove = new Paper();
+        return (new Paper());
     }
     else if (moveName == "Scissors")
     {
-        currentMove = new Scissors();
+        return (new Scissors());
     }
     else if (moveName == "Monkey")
     {
-        currentMove = new Monkey();
+        return (new Monkey());
     }
     else if (moveName == "Robot")
     {
-        currentMove = new Robot();
+        return (new Robot());
     }
     else if (moveName == "Pirate")
     {
-        currentMove = new Pirate();
+        return (new Pirate());
     }
     else if (moveName == "Ninja")
     {
-        currentMove = new Ninja();
+        return (new Ninja());
     }
     else if (moveName == "Zombie")
     {
-        currentMove = new Zombie();
+        return (new Zombie());
     }
-    return currentMove;
+    return nullptr;
 };
