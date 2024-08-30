c375fb1bdb8010c8f8baeed38ef0931d3d4a1823
OmarBadr145 <omary.badr145@gmail.com>
2024-08-30 15:31:50 +0930

    final

--- main.cpp
+++ main.cpp
@@ -8,8 +8,11 @@
 using namespace std;
 int main() {
     BinarySearch Test;
+    QuickSort TestQuicksort;
 
     vector<int> arr = {1,2,3,4,5,6,7,8,9,10,11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    TestQuicksort.sort(arr, 0, n - 1);
     Test.search(arr, 9);
     cout << Test.search(arr, 9) << endl;
 
