be8740d7b0aba75533c23c333bcec5abe2056c9c
OmarBadr145 <omary.badr145@gmail.com>
2024-08-31 16:44:50 +0930

    fixed up quicksort andbubble sort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,7 +5,7 @@ std::vector<int> BubbleSort::sort(std::vector<int> list)
     int i, j;
     for (i = 0; i < list.size(); i++)
     {
-        swapped = false; /// Set swapped to false every time
+        bool swapped = false; /// Set swapped to false every time
         for (j = 0; j < list.size(); j++)
         {
             if (list[j] > list[j + 1])
