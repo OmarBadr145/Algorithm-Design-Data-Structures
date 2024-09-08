1c7fa62823b14ea05afbb304a9ea55b3df7bbf80
OmarBadr145 <omary.badr145@gmail.com>
2024-09-08 15:59:48 +0930

    finished bigNumCalc

---
+++ BigNumCalc.cpp
@@ -0,0 +1,84 @@
+#include "BigNumCalc.h"
+
+bigNumCalc::bigNumCalc(){
+
+}
+bigNumCalc::~bigNumCalc(){
+
+}
+
+std::list<int>bigNumCalc::buildBigNum(std::string numString){
+    std::list<int> numbers;
+    for(int i = 0; i < numString.length(); i++){  ///looping through  string
+        numbers.push_back(numString[i] - '0');
+    }
+    return numbers;
+}
+
+std::list<int>bigNumCalc::add(std::list<int> num1, std::list<int> num2){  
+    std::list<int> result;
+    int carry = 0;
+
+    while((!num1.empty() || !num2.empty()) || carry){
+        int temp = carry;
+
+    ///adding num1 to temp
+        if(!num1.empty()){   
+            temp += num1.back();  //accesing the last element and addign it to temp
+            num1.pop_back();    ///removing last element because we already added it to temp
+        }
+    ///adding num2 to temp
+        if(!num2.empty()){   
+            temp += num2.back();  //accesing the last element and addign it to temp
+            num2.pop_back();    ///removing last element because we already added it to temp
+        }
+        
+        result.push_front(temp % 10);
+        carry = temp/10;
+    }
+    return result;
+}
+
+std::list<int>bigNumCalc::sub(std::list<int> num1, std::list<int> num2){
+    std::list<int> result;
+    int revCarry;
+    
+    while(!num1.empty() || !num2.empty()){
+        int sub = revCarry;
+        if(!num1.empty()){
+            sub += num1.back();   ///adding last digit of num1 to sub
+            num1.pop_back();      ///removing last digit cause already added
+        }
+
+        if(!num2.empty()){
+            sub -= num2.back();
+            num2.pop_back();
+        }
+
+        if (sub < 0){    ///making sure reverse carry operation works
+            sub += 10;
+            revCarry = -1;
+        }
+        else {
+            revCarry = 0;
+        }
+        result.push_front(sub);
+    }
+    while (!result.empty() && result.front() == 0){
+        result.pop_front();
+    }
+    return result;
+}
+
+std::list<int>bigNumCalc::mul(std::list<int> num1, std::list<int> num2){
+    int number = num2.front();   //getting first number of num2
+    num2.pop_front(); ///deleting first element of num2
+
+    std::list <int> result;
+    result.push_back(0);
+
+    for(int i = 0; i < number; i++){
+        result = this-> add(result, num1);
+    }
+    return result; 
+}
