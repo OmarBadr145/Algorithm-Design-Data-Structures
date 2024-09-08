e23f86f2cc571754ed3681ddfb302fc5537b7a0d
OmarBadr145 <omary.badr145@gmail.com>
2024-09-08 16:08:40 +0930

    finished bigNumCalc

--- BigNumCalc.h
+++ BigNumCalc.h
@@ -4,12 +4,13 @@
 #include <string>
 
 class BigNumCalc {
-BigNumCalc();   ///constructor
-~BigNumCalc(); ///destructor
-std::list<int> buildBigNum(std::string numString); ///Creating a list from a provided string, "12345" becomes "1","2"...
-std::list<int> add(std::list<int> num1, std::list<int> num2); ///Adds num1 to num2, return sum
-std::list<int> sub(std::list<int> num1, std::list<int> num2);///Subtract num2 from num1 and return difference
-std::list<int> mul(std::list<int> num1, std::list<int> num2);/// Multipuly num1 by num1 and return product. assume num 2 is one digit long
+public:
+    BigNumCalc();   ///constructor
+    ~BigNumCalc(); ///destructor
+    std::list<int> buildBigNum(std::string numString); ///Creating a list from a provided string, "12345" becomes "1","2"...
+    std::list<int> add(std::list<int> num1, std::list<int> num2); ///Adds num1 to num2, return sum
+    std::list<int> sub(std::list<int> num1, std::list<int> num2);///Subtract num2 from num1 and return difference
+    std::list<int> mul(std::list<int> num1, std::list<int> num2);/// Multipuly num1 by num1 and return product. assume num 2 is one digit long
 };
 
 
