3539e6825d244fc8e030d900da0207617346b878
OmarBadr145 <omary.badr145@gmail.com>
2024-10-02 18:37:22 +0930

    Started

---
+++ TrieNode.h
@@ -0,0 +1,23 @@
+#ifndef TRIENOTE_H
+#define TRIENOTE_H
+#include <iostream>
+#include <string>
+#include <vector>
+
+class TrieNode{
+
+    protected:
+
+bool wordEnd;
+TrieNode * childNode[26];
+int routerNum;
+
+    public:
+TrieNode();
+
+
+
+
+};
+
+#endif
