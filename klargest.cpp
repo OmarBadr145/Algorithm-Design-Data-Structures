e804b84409be92207eb9396463375ed965338c5e
OmarBadr145 <omary.badr145@gmail.com>
2024-10-16 12:00:30 +1030

    commenting klargest

--- klargest.cpp
+++ klargest.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 
 int kth_largest(std::vector<int> values, int k){
-    std::priority_queue<int, std::vector<int>, std::greater<int>> priorityQueue;
-    int n = values.size();
-    for(int i = 0; i < n; i++){
-        priorityQueue.push(values[i]);
+    std::priority_queue<int, std::vector<int>, std::greater<int>> priorityQueue; ///initialising a priority queue
+    int n = values.size();   ///size of values
+    for(int i = 0; i < n; i++){ ///looping through values
+        priorityQueue.push(values[i]);  ///pushing values through the priority queue
     }
 
     for(int i = 0; i < (n-k); i++){
